1d1e958bb8a9bb7fdb84ee47a8a3b79caf3b31c7
Vandy <matthew.vandergrift@homeaidepi.com>
2022-04-12 11:01:42 -0400

    fix python script

--- workloads/beta.py
+++ workloads/beta.py
@@ -1,4 +1,11 @@
 import sys
-print('Hello Beta')
-print('argument 1:"', sys.argv[0], '"')
-print('argument 2:"', sys.argv[1], '"')
+
+def beta(a,b):
+    print("Hello Beta")
+    print('argument 1:"', param1, '"')
+    print('argument 2:"', param2, '"')
+
+if __name__ == "__main__":
+    param1 = sys.argv[1]
+    param2 = sys.argv[2]
+    beta(param1, param2)
