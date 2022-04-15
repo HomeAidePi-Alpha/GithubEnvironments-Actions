b72c5d91b69a813bff7137173a125866067dc6b9
Vandy <matthew.vandergrift@homeaidepi.com>
2022-04-14 23:16:18 -0400

    adding terraform

---
+++ .github/workflows/ci-cd/terraform/azure/main.tf
@@ -0,0 +1,41 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name  = env.AZURE_RESOURCE_GROUP_NAME
+        storage_account_name = env.AZURE_STORAGE_ACCOUNT_NAME
+        container_name       = env.AZURE_STORAGE_CONTAINER_NAME
+        key                  = env.AZURE_STORAGE_CONTAINER_NAME_KEY
+    }
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "2.42.0"
+        }
+    }
+}
+provider "azurerm" {
+    features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "tamops-rg" {
+  name     = env.AZURE_RESOURCE_GROUP
+  location = env.AZURE_REGION
+}
+ 
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tamops-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = env.AZURE_REGION
+  resource_group_name = azurerm_resource_group.tamops.name
+}
+ 
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.tamops.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
+}
