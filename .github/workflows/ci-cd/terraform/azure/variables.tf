00ed61e04a9b6e58059ce0e53caddcd413818b77
Vandy <matthew.vandergrift@homeaidepi.com>
2022-04-18 13:27:47 -0400

    add variables file

---
+++ .github/workflows/ci-cd/terraform/azure/variables.tf
@@ -0,0 +1,23 @@
+variable "AZURE_RESOURCE_GROUP" {
+  type = string
+}
+
+variable "AZURE_RESOURCE_GROUP_NAME" {
+  type = string
+}
+
+variable "AZURE_STORAGE_ACCOUNT_NAME" {
+  type = string
+}
+
+variable "AZURE_STORAGE_CONTAINER_NAME" {
+  type = string
+}
+
+variable "AZURE_STORAGE_CONTAINER_NAME" {
+  type = string
+} 
+
+variable "AZURE_REGION" {
+  type = string
+}
