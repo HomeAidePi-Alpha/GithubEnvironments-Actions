c3a1a0447b9e34fbb1288deb21b6144686c379da
Vandy <matthew.vandergrift@homeaidepi.com>
2022-04-24 22:55:47 -0400

    check len

--- workloads/python/github-actions-delete-workflows-by-owner-repo.py
+++ workloads/python/github-actions-delete-workflows-by-owner-repo.py
@@ -4,6 +4,8 @@ def deleteAllWorkflowRunsByOwnerByRepo(owner,repo, github_token):
     print("Hello Alpha-Actions")
     print('owner:"', owner, '"')
     print('repo:"', repo, '"')
+    print('token len:"', len(token), '"')
+    print('token first3:"', token[1:3], '"')
     from ghapi.all import GhApi
     api = GhApi(token=github_token)
 
