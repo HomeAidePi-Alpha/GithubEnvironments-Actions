7ee72da0042ed36b9519b0bc0238b8adcb9ebec5
Vandy <matthew.vandergrift@homeaidepi.com>
2022-04-25 10:48:41 -0400

    try per_page: 1

--- workloads/python/github-actions-delete-workflows-by-owner-repo.py
+++ workloads/python/github-actions-delete-workflows-by-owner-repo.py
@@ -16,7 +16,7 @@ def deleteAllWorkflowRunsByOwnerByRepo(owner,repo, token, workflow_id):
     workflows = response.workflows
     for workflow in workflows:
         print(workflow)
-        response = api.actions.list_workflow_runs(owner, repo, workflow_file_name=workflow_id)
+        response = api.actions.list_workflow_runs_for_repo(owner, repo, workflow_file_name=workflow_id, per_page = 1,)
         print(response)
         runs = response.workflow_runs
         print(runs)
