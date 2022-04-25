c128ba60fc4677dc36a2c6a302ebfa56b8bd2dde
Vandy <matthew.vandergrift@homeaidepi.com>
2022-04-24 21:28:55 -0400

    update token call to ghapi module

--- workloads/python/github-actions-delete-workflows-by-owner-repo.py
+++ workloads/python/github-actions-delete-workflows-by-owner-repo.py
@@ -1,11 +1,11 @@
 import sys
 
-def deleteAllWorkflowRunsByOwnerByRepo(owner,repo):
+def deleteAllWorkflowRunsByOwnerByRepo(owner,repo, github_token):
     print("Hello Alpha-Actions")
     print('owner:"', owner, '"')
     print('repo:"', repo, '"')
     from ghapi.all import GhApi
-    api = GhApi()
+    api = GhApi(token=github_token)
 
     runs = api.actions.list_workflow_runs_for_repo(owner, repo)
     for run in runs.workflow_runs:
@@ -14,4 +14,5 @@ def deleteAllWorkflowRunsByOwnerByRepo(owner,repo):
 if __name__ == "__main__":
     owner = sys.argv[1]
     repo = sys.argv[2]
-    deleteAllWorkflowRunsByOwnerByRepo(owner, repo)
+    github_token = sys.argv[2]
+    deleteAllWorkflowRunsByOwnerByRepo(owner, repo, github_token)
