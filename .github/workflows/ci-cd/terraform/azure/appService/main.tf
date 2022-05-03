393def8f3550f120abad820c8479b3872a15096f
Vandy <matthew.vandergrift@homeaidepi.com>
2022-05-03 08:47:18 -0400

    try terraform after azure cli

--- .github/workflows/ci-cd/terraform/azure/appService/main.tf
+++ .github/workflows/ci-cd/terraform/azure/appService/main.tf
@@ -51,8 +51,6 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription-id
   tenant_id       = var.tenant-id
-  client_id       = var.client-id
-  client_secret   = var.client-secret
 }
 
 resource "azurerm_resource_group" "alpha" {
