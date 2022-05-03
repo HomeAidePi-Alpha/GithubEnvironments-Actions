bbc3c85c13afb4fa884cbda8999265edf496cc92
Vandy <matthew.vandergrift@homeaidepi.com>
2022-05-03 09:46:15 -0400

    typo

--- .github/workflows/ci-cd/terraform/azure/appService/main.tf
+++ .github/workflows/ci-cd/terraform/azure/appService/main.tf
@@ -52,7 +52,7 @@ provider "azurerm" {
 
   subscription_id = var.subscription-id
   client_id       = var.client-id
-  client_secret   = var.client_secret
+  client_secret   = var.client-secret
   tenant_id       = var.tenant-id
 }
 
