0bc9138d79d8c133b2be7353f3ff65c1b703afab
Vandy <matthew.vandergrift@homeaidepi.com>
2022-05-03 09:50:31 -0400

    make it just tenant and subsc

--- .github/workflows/ci-cd/terraform/azure/appService/main.tf
+++ .github/workflows/ci-cd/terraform/azure/appService/main.tf
@@ -51,8 +51,6 @@ provider "azurerm" {
   features {}
 
   subscription_id = var.subscription-id
-  client_id       = var.client-id
-  client_secret   = var.client-secret
   tenant_id       = var.tenant-id
 }
 
